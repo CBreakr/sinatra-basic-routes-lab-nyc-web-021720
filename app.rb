0e6ab14b8d3ada5e70345e35d16822f3fd848165
CBreakr <github email address>
2020-03-08 22:26:47 -0400

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,28 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+
+    get "/name" do
+        "My name is Rob"
+    end
+
+    get "/name/:name" do
+        "My name is #{params["name"]}"
+    end
+
+    get "/hometown" do 
+        "My hometown is Brooklyn"
+    end
+
+    get "/hometown/:hometown" do 
+        "My hometown is #{params["hometown"]}"
+    end
+
+    get "/favorite-song" do 
+        "My favorite song is ... I couldn't say"
+    end
+
+    get "/favorite-song/:fsong" do 
+        "My favorite song is #{params["fsong"]}"
+    end
 end
